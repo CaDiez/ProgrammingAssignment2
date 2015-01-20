e92e4c5346b3bff75f0d1851ce45b72b08f40ba7
CaDiez <ca_diez@hotmail.com>
2015-01-20 16:59:08 -0600

    Final submission

--- cachematrix.R
+++ cachematrix.R
@@ -13,22 +13,22 @@
 makeCacheMatrix <- function(x = matrix()) {
         ## Initializa Variable
         ##Inicialización de variable
-        Inv<-NULL
-        ## This function is used to set or reset the values of the matrix, if it´s resetted then initializes Inv top NULL
-        ## Funcion utilizada para fijar o reestablecer la matriz, si es reestablecida se reinicializa Inv a NULL
+        InvMat<-NULL
+        ## This function is used to set or reset the values of the matrix, if it´s resetted then initializes InvMat top NULL
+        ## Funcion utilizada para fijar o reestablecer la matriz, si es reestablecida se reinicializa InvMat a NULL
                 Set <- function(y){
                 x<<-y
-                Inv<<-NULL
+                InvMat<<-NULL
         }
         ## Gets the matrix from the list
         ## Obtiene la matriz de la lista
         Get <- function() x
-        ## Writes the Inverse to the environment thru Inv Variable, that later we will use as cache
-        ## Escribe el Invero hacia el ambiente mediante la variable Inv, que posteriormente se usar´pa como caché
-        SetInverse <- function(inverse) Inv<<-inverse
-        ## Function that makes the call to the Inv environment variable or cache
-        ## Funcion que llama a la variable Inv del ambiente ó caché
-        GetInverse <- function() Inv
+        ## Writes the Inverse to the environment thru InvMat Variable, that later we will use as cache
+        ## Escribe el Inverso hacia el ambiente mediante la variable InvMat, que posteriormente se usará como caché
+        SetInverse <- function(inverse) InvMat<<-inverse
+        ## Function that makes the call to the InvMat environment variable or cache
+        ## Funcion que llama a la variable InvMat del ambiente ó caché
+        GetInverse <- function() InvMat
         ## Generates the formatted special list that contains functions and data (matrix) as function´s output
         ## Genera la lista en formato especial que contiene las funciones y los datos (matriz) como salida de la funcion
         list(Set=Set, Get=Get,
@@ -36,26 +36,26 @@ makeCacheMatrix <- function(x = matrix()) {
              GetInverse=GetInverse)
 }
 
-## Function that checks if the inversion calculation has been made, if done, calls Environment Inv variable of cache, 
+## Function that checks if the inversion calculation has been made, if done, calls Environment InvMat variable of cache, 
 ## if does not, execute calculation on the fly
-## Función que revisa si el calculo de inversión fue hecho, si ya, llama la variabe de ambiente Inv ó caché
+## Función que revisa si el calculo de inversión fue hecho, si ya, llama la variabe de ambiente InvMat ó caché
 ## Si no, ejecuta el cálculo al vuelo.
 cacheSolve <- function(x, ...) {
-        ## Checks if the calculation has been done for a set of data, If it is then gets value from the "cache" thru Inv and returns the result
-        ## Revisa si ya se hizo el calculo para el conjunto de datos, Si lo hizo, obtiene el valor del cache a traves de la variable Inv y regresa el resultado
-        Inv<- x$GetInverse()
-        if (!is.null(Inv)){
+        ## Checks if the calculation has been done for a set of data, If it is then gets value from the "cache" thru InvMat and returns the result
+        ## Revisa si ya se hizo el calculo para el conjunto de datos, Si lo hizo, obtiene el valor del cache a traves de la variable InvMat y regresa el resultado
+        InvMat<- x$GetInverse()
+        if (!is.null(InvMat)){
                 message("Getting cache data")
-                return(Inv)
+                return(InvMat)
         }
         ## If the calculation has not been done, then gets the original matrix, calculate it´s inverse, 
-        ## sets Cache thru Inv Variable and return the results
+        ## sets Cache thru InvMat Variable and return the results
         ## Si el cálculo no se ha hecho, entonces se obtiene la matriz original, se calcula el inverso, 
-        ## se establece el Cache a través de la variable Inv y se regresa el resultado
+        ## se establece el Cache a través de la variable InvMat y se regresa el resultado
         data <- x$Get()
-        Inv<- solve(data,...)
-        x$SetInverse(Inv)
-        Inv
+        InvMat<- solve(data,...)
+        x$SetInverse(InvMat)
+        InvMat
 }
 
 ## END OF SET OF FUNCTIONS
