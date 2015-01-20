5f2d7d62e38d5e86710baccafe8e2f114d70e356
CaDiez <ca_diez@hotmail.com>
2015-01-20 15:14:19 -0600

    Completed the main structure of the functions, comments pending

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,26 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Commentaries are shown in English and Spanish
+## Set of Functions designed to create a list that sets an "special" list of functions and data matrix
+## and calculate the inverse of the matrix, if the result have been calculated then is get it from cache if not, calculation on the fly.
 
-## Write a short comment describing this function
+## Conjunto de funciones utilizadas para crear una lista especial de datos de matrices y funciones
+## así como el cálculo del inverso de la matriz, si ya fue calculado se obtiene del cache, si no, se obtiene al vuelo.
+
+
+## Creates an special list containing th matrix and functions to set and get the matrix and its inverse
+## Crea una lista especial que contiene la matriz y funciones para establecer y llamar a la matriz y a su inverso.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        Inv<-NULL
+        Set <- function(y){
+                x<<-y
+                Inv<<-NULL
+        }
+        Get <- function() x
+        SetInverse <- function(inverse) Inv<<-inverse
+        GetInverse <- function() Inv
+        list(Set=Set, Get=Get,
+             SetInverse=SetInverse,
+             GetInverse=GetInverse)
 }
 
 
@@ -12,4 +28,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        Inv<- x$GetInverse()
+        if (!is.null(Inv)){
+                message("getting cache data")
+                return(Inv)
+        }
+        data <- x$Get()
+        Inv<- solve(data,...)
+        x$SetInverse(Inv)
+        Inv
 }
